3e28d19e913d87889c5011a744e7b06e01c30a4e
Ana Clara Matos <anasmatos28@gmail.com>
2022-10-02 21:40:14 -0400

    fix: solved no token error message

--- src/routes/postsRouter.ts
+++ src/routes/postsRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import {getAllPosts, newPost, adopt} from "../controllers/postsController.js"
+import { validateToken } from "../middlewares/validateToken.js";
 
 const postsRouter = Router()
 
-postsRouter.get("/feed", getAllPosts)
+postsRouter.get("/feed", validateToken, getAllPosts)
 postsRouter.post("/create", newPost)
-postsRouter.get("/adopt/:id", adopt)
+postsRouter.get("/adopt/:id", validateToken, adopt)
 
 export default postsRouter;
